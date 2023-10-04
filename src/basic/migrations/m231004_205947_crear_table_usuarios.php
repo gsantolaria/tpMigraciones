8c1c5c93421b12a59a9d732909e3e1d0564db99a
guillermo santolaria <guillote.s07@gmail.com>
2023-10-04 18:09:42 -0300

    1er migracion tabla usuario

---
+++ src/basic/migrations/m231004_205947_crear_table_usuarios.php
@@ -0,0 +1,48 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m231004_205947_crear_table_usuarios
+ */
+class m231004_205947_crear_table_usuarios extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('usuario', [
+            'id' => $this->primaryKey(),
+            'username' => $this->string()->notNull(),
+            'nombre' => $this->text(),
+            'apellido' => $this->text(),
+            'edad' => $this->integer(),
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('usuario');
+
+        return true;
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m231004_205947_crear_table_usuarios cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
